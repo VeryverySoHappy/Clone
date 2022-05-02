73c72adbf02e1696cd9f98299038581765f266df
VeryverySoHappy <frontEnd0815@gmail.com>
2022-05-02 21:51:08 +0900

    kia

--- Worldwide_kia/js/main.js
+++ Worldwide_kia/js/main.js
@@ -1,14 +1,47 @@
+// section02 swiper start
+var swiper = new Swiper(".section02 .mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+// section02 swiper end
+
+// section02 text start
 $(document).ready(function(){
-  $('.story_list li:not(:eq(0)) > .story').hide();
-
-  $('.section03').on({
-    mouseenter:function(){
-      $('.story').mousemove(function(event){
-        var pageCode = event.pageX - $('.story').offset().left;
-        if(pageCode>1920){
-
-        }
-      })
-    }
+  var section02_text = $('.section02').offset().top;
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    // if (scroll)
+    console.log(scrollTop);
   })
 })
+
+if (document.body.scrollTop == 0) {
+  var top = document.body.documentElement.scrollTop;
+  } else {
+  var top = document.body.scrollTop;
+  }
+// section02 text end
+
+// section03 start
+var swiper = new Swiper(".section03 .mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// section03 end
+
